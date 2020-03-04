034c88520d4b394ab57a558bc141e4858c63c830
Homayoun <HomayounSadri@users.noreply.github.com>
2020-03-04 18:20:57 -0500

    edit guessinggame.sh

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 The Unix Workbench Project: Guessing Game
 
 Date and time at which make was run:
-Wed Mar  4 18:08:52 EST 2020
+Wed Mar  4 18:19:58 EST 2020
 
 Number of lines of code in guessinggame.sh is
-      26
+      25
--- guessinggame.sh
+++ guessinggame.sh
@@ -3,24 +3,23 @@
 num_files=$(ls -l | wc -l)
 
 echo "How many files are in the current directory?"
-read ans
-
-# source guessinggame.sh
-# # guessinggame
+read guess
 
 function guessinggame {
 
-  while [[ $num_files -ne $ans ]]
+  while [[ $guess -ne $num_files ]]
   do
-    if [[ $num_files -gt $ans ]]
+    if [[ $guess -gt $num_files ]]
     then
       echo "Your guess was too high. Guess again:"
-      read ans
-    elif [[ $num_files -lt $ans ]]
+      read guess
+    elif [[ $guess -lt $num_files ]]
     then
       echo "Your guess was too low. Guess again:"
-      read ans
+      read guess
     fi
   done
   echo "Congradulations! Your guess is correct."
 }
+
+guessinggame
